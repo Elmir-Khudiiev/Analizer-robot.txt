2135fa2fa4f2dc23fd9412b2b30d75d1d7f244e6
Elmir Khudiiev <Elmir1993Elmir@gmail.com>
2018-07-18 08:42:36 +0300

    add to USE

--- Router.php
+++ Router.php
@@ -1,20 +1,26 @@
 <?php
-// Подключаем файлы библиотеки PHPExel
-require(__DIR__.'/vendor/autoload.php'); // Файл автозагрузки слассов.
-    
 
+// Подключаем файл автозагрузки слассов
+require(__DIR__.'/vendor/autoload.php');
+
+// Пространство имен классов.
+use classes\Robots;
+use classes\Search;
+use classes\Size;
+use classes\Table;
 
 
 $url = $_POST['url']; // Получаем ссылку с формы.
 
-$Robots = new classes\Robots($url);
+$Robots = new Robots($url);
+$domaine = $Robots->corectURL();
 $RobotsContent = $Robots->robotsContent(); // Контент в "robots.txt"
 
-$Search = new classes\Search($RobotsContent);
+$Search = new Search($RobotsContent);
 $SearchHost = $Search->host(); // Поиск Host: в файле "robots.txt"
 $SearchSiteMap = $Search->siteMap(); // Поиск Sitemap: в файле "robots.txt"
 
-$Size = new classes\Size;
+$Size = new Size;
 $RobotSize = $Size->robotSize($RobotsContent, __DIR__.'/temp/robots.txt'); // Проверка веса файла "robots.txt" в (кб).
 unlink(__DIR__.'/temp/robots.txt'); // Удаление временного файла "robots.txt"
 
@@ -27,7 +33,8 @@ $_SESSION['SearchSiteMap'] = $SearchSiteMap;
 $_SESSION['RobotSize'] = $RobotSize;
 $_SESSION['ResponseCode'] = $ResponseCode;
 
-echo '<a class="btn btn-primary" href="classes/Report.php" role="button">Сохранить отчет</a>';
+echo '<a class="btn btn-primary" href="classes/Report.php" role="button">Сохранить отчет</a>'.
+'<a class="btn btn-primary" target="_blank" href="'.$domaine.'" role="button">Просмотреть Robots.txt</a>';
 
-$Table = new classes\Table;
+$Table = new Table;
 $Table->viewTable($RobotsContent, $SearchHost, $RobotSize, $SearchSiteMap, $ResponseCode); // Выводит результат на екран.
